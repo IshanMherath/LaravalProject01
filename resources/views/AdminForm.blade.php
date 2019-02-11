34b0afbda3a3424ce6574c08388a0a708d575f42
IshanMherath <ishanmh009@gmail.com>
2019-02-11 13:50:07 +0530

    First commit

---
+++ resources/views/AdminForm.blade.php
@@ -0,0 +1,124 @@
+
+<link href="//maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css" rel="stylesheet" id="bootstrap-css">
+<script src="//maxcdn.bootstrapcdn.com/bootstrap/4.0.0/js/bootstrap.min.js"></script>
+<script src="//cdnjs.cloudflare.com/ajax/libs/jquery/3.2.1/jquery.min.js"></script>
+<!------ Include the above in your HEAD tag ---------->
+
+
+<html>
+<head>
+<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">
+
+
+
+<!-- jQuery library -->
+<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.2.1/jquery.min.js"></script>
+
+<!-- Latest compiled JavaScript -->
+<script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>
+
+<link rel="stylesheet" href="//maxcdn.bootstrapcdn.com/font-awesome/4.3.0/css/font-awesome.min.css">
+<link rel="stylesheet" href="path/to/font-awesome/css/font-awesome.min.css">
+ 
+<link rel="stylesheet" type="text/css" href="public\css\personal-style.css">
+
+</head>
+
+<body style="background-image:url(https://wallpapertag.com/wallpaper/full/c/a/a/157415-simplistic-wallpapers-1920x1080-lockscreen.jpg);">
+
+	
+
+<div class="container">
+<br/>
+<br/>
+<br/>
+<br/>
+
+<center> <b id="login-name">SIGN-UP Here </b> </center>>
+	
+	<div class="row">
+		
+<div class="col-md-6 col-md-offset-3" id="login">  
+
+<form>
+	
+<div class="form-group">
+<label class="user"> UserName:  </label>
+
+	
+<input type="text" class="form-control" id="text1" name="username" placeholder="Username">
+</div>
+
+
+<div class="form-group">
+<label class="user"> Email ID: </label>
+
+<input type="email" class="form-control" id="text2" name="email" placeholder=" EnterEmail Address">
+
+</div>
+
+<div class="form-group">
+<label class="user"> Password: </label>
+<input type="text" class="form-control" id="text2" name="password" placeholder=" Enter Password">
+</div>
+
+<div class="form-group">
+<label class="user">Confirm Password: </label>
+<input type="text" class="form-control" id="text2" name="ConfirmPassword" placeholder=" Enter Confirm passowrd">
+</div>
+
+
+<div class="form-group">
+
+<input type="submit" class="btn btn-success" value="submit" style="border-radius:0px;">
+<input type="reset" class="btn btn-danger" value="Reset" style="border-radius:0px;">
+
+    </div>
+    <br/><br/><br/>
+    <a href="#" style="color: white; font-size: 15px; float: right; margin-right: 10px;"> Forget Password </a>
+    <a href="#" style="color: white; font-size: 15px; float: right; margin-right: 10px;"> Register </a>
+
+
+
+
+
+
+
+</form>
+
+
+
+
+
+  </div>
+
+
+
+	</div>
+
+
+
+
+
+
+
+
+
+
+</div>
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+	</body>
+	</html>
